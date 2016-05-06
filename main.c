73b2e2a4369ebdc21ef9f258962a4636b6dfe1af
kiko <moomot@ukr.net>
2016-05-06 13:38:48 +0300

    New

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-int main () {
-	int f = summary(2,3);
+int main (int argc, char ** argv) {
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
+	int f = summary(a, b);
 	printf("%d", f);
 	return 0;
 }
